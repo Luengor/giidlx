1d71406ea9669237700acfc54bfc37b227cff3ef
luengor <aluengor0001@gmail.com>
2024-04-23 16:29:54 +0200

    algo

--- serie.py
+++ serie.py
@@ -1,10 +1,29 @@
 #!/usr/bin/env python3
+from sys import argv
 
-nums = [int(input("num > "))]
+if len(argv) > 1:
+    nums = [int(argv[1])]
+else:
+    nums = [int(input("num > "))]
 
 while nums[-1] != 1:
     nums.append(nums[-1] // 2 if nums[-1] % 2 == 0 else nums[-1] * 3 + 1)
 
-print(nums)
-print(len(nums), max(nums), sum(nums) / len(nums)) 
+# Lista 
+vIni = nums[0]
+vT = len(nums)
+vMax = max(nums)
+vMed = sum(nums) / vT
+
+lista = [x * vT for x in [vIni, vMax, vMed, vIni / vMax, vIni / vMed, vMax/vIni, vMax/vMed, vMed/vIni, vMed/vMax]]
+
+print("vIni:", vIni)
+print("vT:", vT)
+print("vMed:", vMed)
+print("vMax:", vMax)
+print("vT/vMax:", vT/vMax)
+print("vT/vIni:", vT/vIni)
+print("vT/vMed:", vT/vMed)
+
+print(lista)
 
